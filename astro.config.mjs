2f8ba85ff16f576683ab0ef687e712bb021a39b0
Joby James <joby.james@citrusinformatics.in>
2025-01-27 09:20:13 +0000

    feat: add mdx file support in Astro and fix side menu display for mdx #10

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,10 @@ import node from "@astrojs/node";
 import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
-
+import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
   output: "server",
   // adapter: deno({
   //   mode: 'middleware' // or 'standalone'
