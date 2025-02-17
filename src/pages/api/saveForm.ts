d7d96d61f5c79602bfa3b8b5b0cc5da5f102b801
Shanil Sasikumar <shanil-sasikumar@netspective.in>
2025-02-17 17:21:24 +0530

    feat: surveilr ingest after lfrom submission

--- src/pages/api/saveForm.ts
+++ src/pages/api/saveForm.ts
@@ -1,17 +1,44 @@
 import { writeFile } from 'fs/promises';
 import type { APIContext } from 'astro';
+import { exec } from "node:child_process";
 
 export async function POST({ request }: APIContext) {
     try {
         const { formData, fileName, userId } = await request.json();
         // Define the file path (modify as needed)
-        let filePath = `./dist/content/lforms/submissions/${userId}.${fileName}.lform-submittion.json`;
-        if (process.env.NODE_ENV == "development") {
-            filePath = `./src/content/lforms/submissions/${userId}.${fileName}.lform-submittion.json`;
-        }
 
+        const filePath = `./src/content/lforms/submissions/${userId}.${fileName}.lform-submittion.json`;
+        const dbIngestPath = `src/content/lforms/submissions`;
+        const dbPath = `src/content/db/lforms`;
         // Write form data to a file
         await writeFile(filePath, JSON.stringify(formData, null, 2), 'utf-8');
+
+        exec(`cd ${dbIngestPath} && surveilr ingest files`, (error, _stdout, stderr) => {
+            if (error) {
+                console.error(`Failed to execute ingest command: ${error.message}`);
+                return;
+            }
+
+            if (stderr.trim()) {
+                console.error(`Ingest command error: ${stderr}`);
+                return;
+            }
+
+            // Move the file only if the ingest command succeeded
+            exec(`mv ${dbIngestPath}/resource-surveillance.sqlite.db ${dbPath}`, (mvError, _mvStdout, mvStderr) => {
+                if (mvError) {
+                    console.error(`Failed to move file: ${mvError.message}`);
+                    return;
+                }
+
+                if (mvStderr.trim()) {
+                    console.error(`Move command error: ${mvStderr}`);
+                    return;
+                }
+
+                console.log(`Merge command executed successfully`);
+            });
+        });
 
         return new Response(JSON.stringify({ message: "Form data saved successfully!" }), {
             status: 200,
