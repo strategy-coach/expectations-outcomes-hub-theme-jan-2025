5832171605de8b358de8369a88784b689dfdad48
Shanil Sasikumar <shanil.sasikumar@citrusinformatics.com>
2025-02-18 15:20:56 +0530

    chore: add loader during form submit

--- src/components/lhc-forms-widget/index.tsx
+++ src/components/lhc-forms-widget/index.tsx
@@ -17,6 +17,7 @@ const LHCFormsWidget: React.FC<LHCFormsWidgetProps> = ({ data, fileName, allowSu
         color: "",
         message: "",
     });
+    const [showLoader, setShowLoader] = useState(false);
 
     const divNotificationRef = useRef<HTMLDivElement | null>(null);
     const formContainerRef = useRef<HTMLDivElement | null>(null);
@@ -32,7 +33,7 @@ const LHCFormsWidget: React.FC<LHCFormsWidgetProps> = ({ data, fileName, allowSu
         const isValid = LForms.Util.checkValidity(elementId);
 
         if (isValid === null) {
-
+            setShowLoader(true);
             const validationResult = LForms.Util.getFormData(
                 elementId,
                 false,
@@ -52,12 +53,14 @@ const LHCFormsWidget: React.FC<LHCFormsWidgetProps> = ({ data, fileName, allowSu
                 const result = await response.json();
                 LForms.Util.addFormToPage(data, formContainerRef.current);
                 setNotification({ show: true, color: 'green', message: result.message });
-
+                setShowLoader(false);
             } catch (error) {
                 setNotification({ show: true, color: 'red', message: `Error saving form data: ${error}` });
+                setShowLoader(false);
             }
         } else {
             setNotification({ show: true, color: 'red', message: 'Please complete all required fields accurately' });
+            setShowLoader(false);
         }
 
         // Scroll to the notification after form submission
@@ -92,11 +95,37 @@ const LHCFormsWidget: React.FC<LHCFormsWidgetProps> = ({ data, fileName, allowSu
                     </button>
                 </div>
             )}
-            <div id="lhc-form-container" ref={formContainerRef}></div>
+            <div className="relative">
+
+                {showLoader ? <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-50 z-10">
+                    <div role="status">
+                        <svg
+                            aria-hidden="true"
+                            className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                            viewBox="0 0 100 101"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentFill"
+                            />
+                        </svg>
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                </div> : undefined}
+                <div id="lhc-form-container" className={showLoader ? "opacity-25" : ""} ref={formContainerRef}></div>
+            </div>
+
             {!allowSubmit &&
                 <div className="w-full text-center "><button
                     onClick={getLHCFormData}
                     type="button"
+                    disabled={showLoader}
                     className="mt-3 text-white bg-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-cyan-600 dark:hover:bg-cyan-700 focus:outline-none dark:focus:ring-cyan-800"
                 >
                     Submit
