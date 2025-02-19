7ca2880a4303168ee1b05edc2145029a4a97e9ec
Shanil Sasikumar <shanil-sasikumar@netspective.in>
2025-02-19 10:45:37 +0530

    chore: update the pnpm commands for user sync

--- support/services/createDBFromModels.ts
+++ support/services/createDBFromModels.ts
@@ -55,7 +55,7 @@ exec(`sqlite3 ${databasePath} ""`, (error, _stdout, stderr) => {
 
                 console.log(`models.auto.sql removed successfully`);
 
-                exec(`cd src/content/db && surveilr admin merge -p "activity%" -p "message%" -p "communication%" -p "contact%" -p "channel%" -p "reaction%" -p "attachment%" && rm -rf resource-surveillance.sqlite.db && mv resource-surveillance-aggregated.sqlite.db resource-surveillance.sqlite.db`, (error, _stdout, stderr) => {
+                exec(`cd src/content/db && surveilr admin merge -p "activity%" -p "message%" -p "communication%" -p "contact%" -p "channel%" -p "reaction%" -p "attachment%" && rm -rf resource-surveillance.sqlite.db && mv resource-surveillance-aggregated.sqlite.db resource-surveillance.sqlite.db && rm -rf models.db`, (error, _stdout, stderr) => {
                     if (error) {
                         console.error(`Failed to execute merge command: ${error.message}`);
                         return;
