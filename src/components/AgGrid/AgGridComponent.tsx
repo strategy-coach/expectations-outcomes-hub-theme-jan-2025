28edee52d7e90de42657dd9e51f085529d9d0e4b
Rejina K P <rejina.kp@citrusinformatics.com>
2025-03-19 14:37:43 +0530

    chore: update security report

--- src/components/AgGrid/AgGridComponent.tsx
+++ src/components/AgGrid/AgGridComponent.tsx
@@ -3,24 +3,10 @@ import { AgGridReact } from "ag-grid-react";
 import type { SortDirection } from "ag-grid-community";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
-import type { RowClickedEvent } from "ag-grid-community";
-//import type { NoDataList } from "../../../lib/native/portfolio/portfolioServices";
-//import DetailPage from "../policies/detailPage/detailPage";
-//import toolTipJson from "@/components/native/tooltip/on_screen_tooltip_information.json";
-//import { getToolTipMessage } from "support/services/service";
-//import ToolTipComponent from "../tooltip/toolTipComponent";
+
 
 type DataItem = Record<string, string | null | undefined>;
 
-interface ParamsType {
-  data: {
-    Issues: number;
-    url: string;
-    Description: string;
-    title: string;
-  };
-  value: number;
-}
 
 interface DefaultColDefType {
   sortable?: boolean;
@@ -28,110 +14,137 @@ interface DefaultColDefType {
 }
 
 interface Props {
-  rowData: DataItem[] ;
+  rowData: DataItem[];
   path?: string;
+  connection?: string;
   autoStyle?: boolean;
+  filter?: boolean;
   page?: string;
   isClick?: boolean;
   defaultColDef?: DefaultColDefType;
 }
-interface EventData {
-  name?: string;
-  person_name?: string;
-  // Add other properties as needed
-}
 const AgGridComponent: React.FC<Props> = ({
   rowData,
-  path,
-  autoStyle,
-  page,
-  isClick,
+  connection,
+  filter,
   defaultColDef,
 }) => {
-  //const [detailPage, setDetailPage] = useState();
-  
+
+  const [data, setData] = useState(rowData)
+  const [activeFilter, setActiveFilter] = useState<string | null>()
+
+  const filterData = (level: string) => {
+    let filteredData = rowData;
+    if (level === activeFilter) {
+      setData(rowData);
+      setActiveFilter("");
+    } else {
+      if (connection === "sqlmap") {
+        filteredData = rowData.filter((data) => data.level === level);
+      } else if (connection === "zap") {
+        filteredData = rowData.filter((data) => data["Risk Level"]?.split('(')[0].trim() === level);
+      }
+
+      setData(filteredData);
+      setActiveFilter(level);
+    }
+  };
+
+
+
   const columnDefs =
-  rowData?.length > 0
-    ? Object.keys(rowData[0])
-        .filter(
-          (key) =>
-            key !== "tag" &&
-            key !== "organization_id" &&
-            key !== "asset_service_type_id" &&
-            key !== "url",
-        )
+    rowData?.length > 0
+      ? Object.keys(rowData[0])
         .map((key: string) => ({
           headerName: String(key).toUpperCase().replaceAll("_", " "),
           field: key,
-          cellRenderer: (params: ParamsType) =>
-            key === "Issues" ? (
-              <a
-                href={`${params.data.url.replace(
-                  "api.github.com/repos",
-                  "github.com",
-                )}`}
-                target="_blank"
-              >
-                {"#" + params.data.Issues}
-              </a>
-            ) : (
-              params.value
-            ),
         }))
-    : []; // ✅ If rowData is empty, return an empty array
+      : [];
 
 
   const paginationPageSize = 10;
 
-  const gridOptions = {
-    onRowClicked: (event: RowClickedEvent) => handleclick(event),
-  };
 
-  // eslint-disable-next-line unicorn/consistent-function-scoping
-  function handleclick(event: RowClickedEvent): void {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-
-    // if (page == "policies") {
-    //   setDetailPage(event.data);
-    // } else {
-      const name =
-        (event.data as EventData)?.name ??
-        (event.data as EventData)?.person_name
-          ?.replaceAll(" ", "-")
-          .toLowerCase();
-
-      window.location.href = `${path}/${name}`;
-    //}
-  }
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   const exportData = () => {
     const params = {
       fileName: "exportedData",
-      // Customize other export options as needed
     };
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+
     const gridApi = gridRef.current.api;
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+
     gridApi.exportDataAsCsv(params);
   };
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const gridRef = React.useRef<any>(null);
+
 
   return (
     <div>
+
+      {filter && connection == "zap" && <div className="flex gap-4">
+        <div
+          onClick={() => filterData("High")}
+          className={`border cursor-pointer rounded-md w-24 h-10 flex items-center justify-center font-semibold text-sm
+      ${activeFilter === "High" ? "bg-red-500 text-white" : "bg-gray-200 text-black"}
+    `}>
+          High
+        </div>
+        <div
+          onClick={() => filterData("Medium")}
+          className={`border cursor-pointer rounded-md w-24 h-10 flex items-center justify-center font-semibold text-sm
+      ${activeFilter === "Medium" ? "bg-orange-500 text-white" : "bg-gray-200 text-black"}
+    `}>
+          Medium
+        </div>
+        <div
+          onClick={() => filterData("Low")}
+          className={`border cursor-pointer rounded-md w-24 h-10 flex items-center justify-center font-semibold text-sm
+      ${activeFilter === "Low" ? "bg-yellow-400 text-white" : "bg-gray-200 text-black"}
+    `}>
+          Low
+        </div>
+        <div
+          onClick={() => filterData("Informational")}
+          className={`border cursor-pointer rounded-md w-32 h-10 flex items-center justify-center font-semibold text-sm
+      ${activeFilter === "Informational" ? "bg-blue-700 text-white" : "bg-gray-200 text-black"}
+    `}>
+          Informational
+        </div>
+      </div>
+      }
+
+      {filter && connection == "sqlmap" &&
+        <div className="flex gap-4">
+          <div
+            onClick={() => filterData("INFO")}
+            className={`border cursor-pointer rounded-md w-24 h-10 flex items-center justify-center font-semibold text-sm
+      ${activeFilter === "INFO" ? "bg-green-600 text-white" : "bg-gray-200 text-black"}
+    `}>
+            Info
+          </div>
+          <div
+            onClick={() => filterData("CRITICAL")}
+            className={`border cursor-pointer rounded-md w-24 h-10 flex items-center justify-center font-semibold text-sm
+      ${activeFilter === "CRITICAL" ? "bg-red-500 text-white" : "bg-gray-200 text-black"}
+    `}>
+            Critical
+          </div>
+          <div
+            onClick={() => filterData("WARNING")}
+            className={`border cursor-pointer rounded-md w-24 h-10 flex items-center justify-center font-semibold text-sm
+      ${activeFilter === "WARNING" ? "bg-orange-500 text-white" : "bg-gray-200 text-black"}
+    `}>
+            Warning
+          </div>
+        </div>
+      }
+
       <button
         type="button"
         onClick={exportData}
         className="mt-3 mb-4 relative inline-flex items-center gap-x-1.5 rounded bg-black px-5 py-2 font-normal text-white ring-1 ring-inset ring-black hover:bg-black focus:z-10 text-base"
       >
-        {/* <ToolTipComponent
-          text="Export Data"
-          message={getToolTipMessage(
-            "export data button",
-            toolTipJson.portfolio.porfolioAssetTooltip,
-          )}
-        /> */}
+
 
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -148,17 +161,16 @@ const AgGridComponent: React.FC<Props> = ({
           ></path>
         </svg>
       </button>
-      
-     
+
+
       <div className="ag-theme-alpine">
         {
           <AgGridReact
-            gridOptions={ gridOptions }
             columnDefs={columnDefs}
             autoSizeStrategy={
-              autoStyle ?? false ? { type: "fitGridWidth" } : undefined
+              { type: "fitGridWidth" }
             }
-            rowData={rowData}
+            rowData={data}
             domLayout="autoHeight"
             pagination={true}
             paginationPageSize={paginationPageSize}
@@ -166,7 +178,7 @@ const AgGridComponent: React.FC<Props> = ({
             defaultColDef={defaultColDef}
           />
         }
-        
+
       </div>
     </div>
   );
