c423f13ebfeb6ea244a02d7277a51e3295fa32e3
Rameesa96 <prameesa96@gmail.com>
2025-03-26 16:29:08 +0530

    chore: update user list page

--- src/components/AgGrid/AgGridComponent.tsx
+++ src/components/AgGrid/AgGridComponent.tsx
@@ -3,6 +3,7 @@ import { AgGridReact } from "ag-grid-react";
 import type { SortDirection } from "ag-grid-community";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+import { DeleteUser } from "../../services/zitadel.services.ts";
 
 
 type DataItem = Record<string, string | null | undefined>;
@@ -51,15 +52,60 @@ const AgGridComponent: React.FC<Props> = ({
   };
 
 
-
   const columnDefs =
     rowData?.length > 0
-      ? Object.keys(rowData[0])
-        .map((key: string) => ({
-          headerName: String(key).toUpperCase().replaceAll("_", " "),
-          field: key,
-        }))
+      ? Object.keys(rowData[0]).map((key: string) => ({
+        headerName: key === "user-list-userId" ? "Delete User" : String(key).toUpperCase().replaceAll("_", " "),
+        field: key,
+        cellRenderer: (params: any) => {
+          if (key === "user-list-userId") {
+            const name = params.data["display name"];
+            return (
+              // deno-lint-ignore jsx-button-has-type
+              <button className="" onClick={() => handleDeleteConfirmation(params.value, name)}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            );
+          }
+          return params.value; // Return the actual value for other fields
+        },
+      }))
       : [];
+
+
+
+  const handleDeleteConfirmation = async (
+    id: string,
+    name: string
+  ): Promise<void> => {
+    const isConfirmed = globalThis.confirm(
+      `Are you sure you want to delete the user ${name}?`,
+    );
+
+    if (isConfirmed) {
+      await DeleteUser(id);
+      const event = new CustomEvent("delete-user", {
+        detail: {
+          description: "Delete User",
+        },
+      });
+      document.dispatchEvent(event);
+    }
+    const currentWindow: Window = window;
+    currentWindow.location.reload();
+  };
+
 
 
   const paginationPageSize = 10;
