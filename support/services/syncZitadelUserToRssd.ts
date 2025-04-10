1039965f60745c7863c6b3bb214ee40acf0c312b
Rameesa96 <prameesa96@gmail.com>
2025-04-10 11:34:27 +0530

    chore: update organization roles in db

--- support/services/syncZitadelUserToRssd.ts
+++ support/services/syncZitadelUserToRssd.ts
@@ -128,7 +128,7 @@ export type Organization = z.infer<typeof OrganizationSchema>;
 /**
  * Constants for data mapping.
  */
-const organizationRoleTypes = [{ organization_role_type_id: 1, code: "ADMIN", value: "admin" }, { organization_role_type_id: 2, code: "USER", value: "user" }];
+const organizationRoleTypes = [{ organization_role_type_id: 1, code: "NETSPECTIVE-ADMIN", value: "netspective-admin" }, { organization_role_type_id: 2, code: "NETSPECTIVE-STAFF", value: "netspective-staff" }, { organization_role_type_id: 3, code: "CUSTOMER-ADMIN", value: "customer-admin" }, { organization_role_type_id: 4, code: "CUSTOMER-STAFF", value: "customer-staff" }];
 
 const contactTypes = [
     { contact_type_id: 1, code: "HOME_ADDRESS", value: "Home Address" },
