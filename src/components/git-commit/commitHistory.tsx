087c9d85264760404e0ff77c18908fcb9a7e78c5
Rejina K P <rejina.kp@citrusinformatics.com>
2025-04-17 11:09:35 +0530

    chore: update page history icon

--- src/components/git-commit/commitHistory.tsx
+++ src/components/git-commit/commitHistory.tsx
@@ -66,24 +66,8 @@ const GitCommitDetails: React.FC<GitCommitDetailsProps> = ({ filename }) => {
                 <div className="relative pb-6">
                   <span className="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200" />
                   <div className="relative flex space-x-3">
-                    <div>
-                      <span className="h-8 w-8 p-1 rounded-full bg-gray-200 flex items-center justify-center ring-8 ring-white">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="w-6 h-6"
-                          style={{ color: "black" }}
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                          />
-                        </svg>
-                      </span>
+                    <div>                    
+                      <span className="h-8 w-8 p-1 rounded-full flex items-center justify-center ring-8 ring-white bg-blue-500">📄</span>
                     </div>
                     <div className="flex min-w-0 flex-1 justify-between space-x-4">
                       <div>
