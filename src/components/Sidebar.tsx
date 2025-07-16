5bed785a17041027b409132021af81ef50060f5f
sreejith <sreejith-kalarikkal@netspective.in>
2025-07-16 12:43:08 +0530

    chore: to fix the multiple arrow in tree menu in ipad  #40

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -51,6 +51,7 @@ const renderMenu = (menu: MenuNode[], slugval: string, level: number = 0) => {
             ) : (
               <details open={isMenuOpen([item], slugval)}>
                 <summary
+                style={{ listStyleType: 'none' }}
                   className={`cursor-pointer flex fst_mn list-none ${
                     isSelected ? "bg-gray-300 dark:bg-gray-700" : ""
                   }`}
